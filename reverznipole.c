e03a4facaac8fa7d6ea5a61046a67e00bec257cd
Filip Kříž <96353861+jokerfilip05@users.noreply.github.com>
2022-05-30 19:18:32 +0200

    Create reverznipole.c

---
+++ reverznipole.c
@@ -0,0 +1,23 @@
+#include<stdio.h>
+
+int main()
+{
+    int c, d, n, a[100], b[100];
+    printf("\n\nZadejte počet prvků v poli :");
+    scanf("%d", &n);
+    printf("\n\nZadej %d prvky\n", n);
+
+    for(c = 0; c < n; c++)
+        scanf("%d", &a[c]);
+
+    for(c = n-1, d = 0; c >= 0; c--, d++)
+        b[d] = a[c];
+
+    for(c = 0; c < n; c++)
+        a[c] = b[c];
+
+    printf("\n\n Výsledné pole je: ");
+    for(c = 0; c < n; c++)
+        printf("%d", a[c]);
+    return 0;
+}
