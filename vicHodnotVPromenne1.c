f3716534698953f499f3a43f8cc92266b2bdd59c
Filip Kříž <96353861+jokerfilip05@users.noreply.github.com>
2022-05-30 18:54:06 +0200

    Create vicHodnotVPromenne1.c

---
+++ vicHodnotVPromenne1.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+#include <math.h>
+#include <stdlib.h>
+#include <time.h>
+
+int main() {
+    int p[3] = {5,42, 3};
+    printf("%d %d %d", p[0], p[1], p[2]);
+    return 0;
+}
