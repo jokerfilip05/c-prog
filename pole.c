be4f3b500d3a52a287e156913bb06319e40b29d2
Filip Kříž <96353861+jokerfilip05@users.noreply.github.com>
2022-05-30 18:47:57 +0200

    Create pole.c

---
+++ pole.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+#include <time.h>
+
+int main() {
+    int pole[3][3];
+    pole[0][0] = 42;
+    printf("%d", pole[0][0]);
+    return 0;
+}
